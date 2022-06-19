ea222771e4b7645c06a4ff2c6208e071943a9a77
Lucas Lima <105889568+Lucas-Lima-22@users.noreply.github.com>
2022-06-19 18:28:44 -0300

    att script

--- aula14/ex016/script.js
+++ aula14/ex016/script.js
@@ -1,28 +1,30 @@
 function contar(){
-    var inicio = Number(document.getElementById('txtinicio').value)
-    var fim = Number(document.getElementById('txtfim').value)
-    var passo = Number(document.getElementById('txtpasso').value)
+    var inicio = document.getElementById('txtinicio').value
+    var num_inicio = Number(inicio)
+    var fim = document.getElementById('txtfim').value
+    var num_fim = Number(fim)
+    var passo = document.getElementById('txtpasso').value
+    var num_passo = Number(passo)
     var res = document.getElementById('res')
-    if (document.getElementById('txtinicio').value.length == 0 || document.getElementById('txtfim').value.length == 0 || document.getElementById('txtpasso').value.length == 0){
+    
+    if (inicio.length == 0 || fim.length == 0 || passo.length == 0){
         window.alert('[ERRO] Faltam Dados!')
     } else {
-        if(passo == 0){
+        if(num_passo == 0){
             window.alert('Passo Inválido! Considerando passo 1')
-            passo = 1
+            num_passo = 1
         }
-        if (inicio < fim){
-            res.innerHTML = 'Contando: <br>'
-            while (inicio <= fim) {
-                res.innerHTML += `${inicio} → `
-                inicio += passo
-        } res.innerHTML += 'FIM'
+        res.innerHTML = 'Contando: <br>'
+        if (num_inicio < num_fim){
+            while (num_inicio <= num_fim) {
+                res.innerHTML += `${num_inicio} → `
+                num_inicio += num_passo
+        }
         } else {
-            res.innerHTML = 'Contando: <br>'
-            while (inicio >= fim) {
-                res.innerHTML += `${inicio} → `
-                inicio -= passo
-            } res.innerHTML += 'FIM'
-        }
-        
+            while (num_inicio >= num_fim) {
+                res.innerHTML += `${num_inicio} → `
+                num_inicio -= num_passo
+            }
+        } res.innerHTML += 'FIM'
     }
 }
