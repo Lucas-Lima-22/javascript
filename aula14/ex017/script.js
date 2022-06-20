ecd78b758a3dc8af7cabe145d590e023ec529f8a
Lucas Lima <105889568+Lucas-Lima-22@users.noreply.github.com>
2022-06-20 16:30:16 -0300

    att ex017

--- aula14/ex017/script.js
+++ aula14/ex017/script.js
@@ -1,26 +1,28 @@
-function gerador() {
-    var num = Number(document.querySelector('#txtnum').value)
-    var res = document.querySelector('#res')
-    if(document.querySelector('#txtnum').value.length == 0){
-        window.alert('Porfavor, digite um número!')
-    } else{
-        res.innerHTML = ''
-        for(c = 1; c <= 10; c++){
-        res.innerHTML += `${num} x ${c} = ${num*c}<br>`
-    }
+function gerador(){
+    var table = document.createElement('table')
+    var thead = document.createElement('thead')
+    var tbody = document.createElement('tbody')
+    var num = document.getElementById('txtnum').value
+    var res = document.getElementById('res')
+
+    table.appendChild(thead)
+    table.appendChild(tbody)
+
+    var tr = document.createElement('tr')
+    var th = document.createElement('th')
+    th.innerHTML = `Tabuada do ${num}`
+
+    tr.appendChild(th)
+    thead.appendChild(tr)
+
+    for(c = 1; c <= 10; c++){
+        var tr = document.createElement('tr')
+        var td = document.createElement('td')
+        td.innerHTML = `${num} x ${c} = ${num*c}`
+
+        tr.appendChild(td)
+        tbody.appendChild(tr)
+        res.innerText = ''
+        res.appendChild(table)
     }
 }
-
-    /*
-    c = 1
-    while (c <= 10) {
-        res.innerHTML += `${num} x ${c} = ${num*c}<br>`
-        c++
-    }
-    ---------------------------------------------------
-    c = 1
-    do { 
-        res.innerHTML += `${num} x ${c} = ${num*c}<br>`
-        c++
-    } while (c <= 10)
-    */
