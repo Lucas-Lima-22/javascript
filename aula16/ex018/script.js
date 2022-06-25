ac752de550bcd0d077aafd3cc978d6c4347ffe6b
Lucas Lima <105889568+Lucas-Lima-22@users.noreply.github.com>
2022-06-25 19:35:02 -0300

    att ex018

--- aula16/ex018/script.js
+++ aula16/ex018/script.js
@@ -1,8 +1,8 @@
-valores = []
-soma_valores = 0
+let valores = []
+let soma_valores = 0
 
 function adicionar(){
-    let num = document.getElementById('txtnum').value
+    let num = Number(document.getElementById('txtnum').value)
     if(valores.includes(num) || num.length == 0 || num < 1 || num > 100){
         window.alert('Valor inválido ou já encontrado na lista.')
     } else{
@@ -10,33 +10,32 @@ function adicionar(){
         let option = document.createElement('option')
         option.innerHTML += `Valor ${num} adicionado. <br>`
         select.appendChild(option)
+        if (valores.length == 0){
+            maior = num
+            menor = num
+        } else{
+            if (num > maior){
+                maior = num
+            }
+            if (num < menor) {
+                menor = num
+            }
+        }
         document.getElementById('txtnum').value = ''
         document.getElementById('txtnum').focus()
         valores.push(num)
         soma_valores += Number(num)
-        document.getElementById('total').style.display = 'none'
+        document.getElementById('total').innerText = ''
     }
-    /*
-    } else{
-        let res = document.getElementById('res')
-        res.innerHTML += `Valor ${num} adicionado. <br>`
-        document.getElementById('txtnum').value = ''
-        document.getElementById('txtnum').focus()
-        valores.push(num)
-        soma_valores += Number(num)
-        document.getElementById('total').style.display = 'none'
-    }
-    */
-    
 }
 
 function finalizar(){
     let total = document.getElementById('total')
     total.innerHTML = `
     Ao todo, temos ${valores.length} números cadastrados. <br> 
-    O maior valor informado foi ${valores.sort().reverse()[0]}. <br>
-    O menor valor informado foi ${valores.sort()[0]}. <br>
+    O maior valor informado foi ${maior}. <br>
+    O menor valor informado foi ${menor}. <br>
     Somando todos os valores temos ${soma_valores}. <br>
     A média dos valores digitados é ${soma_valores / valores.length}.`
-    document.getElementById('total').style.display = 'block'
 }
+
